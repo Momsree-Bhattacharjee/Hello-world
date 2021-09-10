e293e0a846a3cf157d85348be9ae9f3dd18b8062
Momsree-Bhattacharjee <90470723+Momsree-Bhattacharjee@users.noreply.github.com>
2021-09-10 22:26:21 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#displaying "Hello world"
+
+
+# In[ ]:
+
+
+#print is a Python defined default function
+
+
+# In[2]:
+
+
+print ("Hello world")
+
+
+# In[ ]:
+
+
+
+
